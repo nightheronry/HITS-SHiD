62d6213d3a1a94e5cb919b8bee71ccfb4a0d357f
Po-fang (Daniel) Hsu <daniel_hsu@smail.nchu.edu.tw>
2017-02-16 16:44:34 +0800

    Add function that extractHG.py can use  highlights not merged.

--- extractHG.py
+++ extractHG.py
@@ -23,21 +23,28 @@ class extractHG():
 
 
 
-    def timeModify(self, timelist=[]):
+    def timeModify(self, timelist=[], merged_bool=0):
         modetimeList = []
 
-        #print(self.FileName)
+        # print(self.FileName)
         for time in timelist:
             templist = []
             for time2 in time:
                 templist.append([int(time2 / 60), int(time2 % 60)])
             modetimeList.append(templist)
         self.modetime = modetimeList
+        self.merged = merged_bool
         pass
 
     def extractAnnotaton(self, shift=0, exportfile=''):
+        if self.merged  == 0:
+            merged_directory = ''
+        else:
+            merged_directory = 'Merge/'
+
+
         ReadFromAnnotation = open('Annotationlist/Annotationlist_'+self.FileName+'.txt', 'r')
-        WriteToTxt = open('Annotationlist/Merge/'+exportfile+'/HighlightAnnotation_'+self.FileName+'_'+exportfile+'.txt', 'w')
+        WriteToTxt = open('Annotationlist/' + merged_directory+exportfile+'/HighlightAnnotation_'+self.FileName+'_'+exportfile+'.txt', 'w')
         starttime = ReadFromAnnotation.readline().rstrip().split(':')
         starttime[0] = int(starttime[0])
         starttime[1] = int(starttime[1])
@@ -50,7 +57,7 @@ class extractHG():
                 templist.append((a if len(a) > 1 else '0'+a)+':'+(b if len(b) > 1 else '0'+b))
             highlighttimes.append(templist)
         for highlighttime in highlighttimes:
-            #print(highlighttimes)
+            # print(highlighttimes)
             Alluserplayer = []
             Alluseraction = []
             Poweruserplayer = []
@@ -61,12 +68,12 @@ class extractHG():
             for line in ReadFromAnnotation:
                 # print(line)
                 if line.rstrip() in highlighttime:
-                    #print(line.rstrip())
+                    # print(line.rstrip())
                     ReadFromAnnotation.readline().rstrip()
-                    Alluserplayer = tuplemerge(Alluserplayer, ast.literal_eval(ReadFromAnnotation.readline().rstrip()))#+= ast.literal_eval(ReadFromAnnotation.readline().rstrip())
-                    #print(Alluserplayer)
+                    Alluserplayer = tuplemerge(Alluserplayer, ast.literal_eval(ReadFromAnnotation.readline().rstrip()))
+                    # print(Alluserplayer)
                     ReadFromAnnotation.readline()
-                    Alluseraction = tuplemerge(Alluseraction, ast.literal_eval(ReadFromAnnotation.readline().rstrip()))#+= ast.literal_eval(ReadFromAnnotation.readline().rstrip())
+                    Alluseraction = tuplemerge(Alluseraction, ast.literal_eval(ReadFromAnnotation.readline().rstrip()))
                     ReadFromAnnotation.readline()
                     ReadFromAnnotation.readline()
                     Poweruserplayer = tuplemerge(Poweruserplayer, ast.literal_eval(ReadFromAnnotation.readline().rstrip()))
@@ -79,7 +86,7 @@ class extractHG():
                     Normaluseraction = tuplemerge(Normaluseraction, ast.literal_eval(ReadFromAnnotation.readline().rstrip()))
 
             WriteToTxt.write(str(highlighttime)+'\n')
-            #print(highlighttime)
+            # print(highlighttime)
             WriteToTxt.write('Alluser\n')
             WriteToTxt.write(str(sorted(Alluserplayer, key=lambda x: x[1], reverse=True)) + '\n')
             WriteToTxt.write('\n')
@@ -105,69 +112,80 @@ if __name__ == '__main__':
 
     #  3_05
     ex = extractHG('3_05')
-    # ex.timeModify([53, 75, 23, 83, 165, 54])
-    ex.timeModify([[53, 54], [75], [23], [83], [165]])
-
+    ex.timeModify([[53], [75], [23], [83], [165], [54]])
     ex.extractAnnotaton(0, 'HITS')
 
-    # ex.timeModify([53, 23, 165, 75, 54, 18, 83, 74, 17, 13, 46, 11, 22, 37, 137])
-    ex.timeModify([[53, 54], [22, 23], [165], [74, 75], [18], [83], [17], [13], [46], [11], [37], [137]])
+    ex.timeModify([[53, 54], [75], [23], [83], [165]], 1)
+    ex.extractAnnotaton(0, 'HITS')
+
+    ex.timeModify([[53], [23], [165], [75], [54], [18], [83], [74], [17], [13], [46], [11], [22], [37], [137]])
+    ex.extractAnnotaton(0, 'MT')
+    ex.timeModify([[53, 54], [22, 23], [165], [74, 75], [18], [83], [17], [13], [46], [11], [37], [137]], 1)
     ex.extractAnnotaton(0, 'MT')
 
     #  3_06
     ex = extractHG('3_06')
-    # ex.timeModify([58, 62, 59, 126, 184, 135, 54, 125, 55, 49, 63])
-    ex.timeModify([[58, 59], [62, 63], [125, 126], [184], [135], [54, 55], [49]])
+    ex.timeModify([[58], [62], [59], [126], [184], [135], [54], [125], [55], [49], [63]])
+    ex.extractAnnotaton(0, 'HITS')
+    ex.timeModify([[58, 59], [62, 63], [125, 126], [184], [135], [54, 55], [49]], 1)
     ex.extractAnnotaton(0, 'HITS')
     # 58->37:20
-    # ex.timeModify([58, 62, 59, 184, 126, 48, 55, 135, 19, 54, 49, 51, 50, 63, 182])
-    ex.timeModify([[58, 59], [62, 63], [184], [126], [48, 49, 50, 51], [54, 55], [135], [19], [182]])
+    ex.timeModify([[58], [62], [59], [184], [126], [48], [55], [135], [19], [54], [49], [51], [50], [63], [182]])
+    ex.extractAnnotaton(0, 'MT')
+    ex.timeModify([[58, 59], [62, 63], [184], [126], [48, 49, 50, 51], [54, 55], [135], [19], [182]], 1)
     ex.extractAnnotaton(0, 'MT')
 
     # 7_09
     ex = extractHG('7_09')
-    # ex.timeModify([71, 75, 70, 72, 69, 141, 76, 131, 68, 152, 57, 132])
-    ex.timeModify([[68, 69, 70, 71, 72], [75, 76], [141], [131, 132], [152], [57]])
-
+    ex.timeModify([[71], [75], [70], [72], [69], [141], [76], [131], [68], [152], [57], [132]])
+    ex.extractAnnotaton(0, 'HITS')
+    ex.timeModify([[68, 69, 70, 71, 72], [75, 76], [141], [131, 132], [152], [57]], 1)
     ex.extractAnnotaton(0, 'HITS')
 
-    # ex.timeModify([71, 70, 72, 75, 69, 57, 76, 68, 141, 56, 73, 131, 58, 152, 142])
-    ex.timeModify([[68, 69, 70, 71, 72, 73], [75, 76], [56, 57, 58], [141, 142], [131], [152]])
+    ex.timeModify([[71], [70], [72], [75], [69], [57], [76], [68], [141], [56], [73], [131], [58], [152], [142]])
+    ex.extractAnnotaton(0, 'MT')
+    ex.timeModify([[68, 69, 70, 71, 72, 73], [75, 76], [56, 57, 58], [141, 142], [131], [152]], 1)
 
     ex.extractAnnotaton(0, 'MT')
 
     #  7_10
     ex = extractHG('7_10')
-    # ex.timeModify([133, 198, 130, 195, 199, 146, 183, 196, 149, 201, 137, 189])
-    ex.timeModify([[133], [198, 199], [130], [195, 196], [146], [183], [149], [201], [137], [189]])
+    ex.timeModify([[133], [198], [130], [195], [199], [146], [183], [196], [149], [201], [137], [189]])
+    ex.extractAnnotaton(0, 'HITS')
+    ex.timeModify([[133], [198, 199], [130], [195, 196], [146], [183], [149], [201], [137], [189]], 1)
 
     ex.extractAnnotaton(0, 'HITS')
 
-    # ex.timeModify([133,198,199,130,195,201,134,200,196,190,146,149,51,189,153])
-    ex.timeModify([[133], [198, 199, 200], [130], [195, 196], [134], [189, 190], [146], [149], [51], [153]])
+    ex.timeModify([[133], [198], [199], [130], [195], [201], [134], [200], [196], [190], [146], [149], [51], [189], [153]])
+    ex.extractAnnotaton(0, 'MT')
+    ex.timeModify([[133], [198, 199, 200], [130], [195, 196], [134], [189, 190], [146], [149], [51], [153]], 1)
 
     ex.extractAnnotaton(0, 'MT')
 
     #  7_13
     ex = extractHG('7_13')
-    # ex.timeModify([157, 64, 50, 65, 85, 86, 51, 161, 89, 158])
-    ex.timeModify([[157, 158], [64, 65], [50, 51], [85, 86], [161], [89]])
+    ex.timeModify([[157], [64], [50], [65], [85], [86], [51], [161], [89],[158]])
+    ex.extractAnnotaton(0, 'HITS')
+    ex.timeModify([[157, 158], [64, 65], [50, 51], [85, 86], [161], [89]], 1)
 
     ex.extractAnnotaton(0, 'HITS')
 
-    # ex.timeModify([64, 65, 50, 157, 51, 86, 85, 135, 158, 89, 161, 136, 52, 66, 162])
-    ex.timeModify([[64, 65, 66], [50, 51, 52], [157, 158], [85, 86], [135], [89], [161, 162], [136]])
+    ex.timeModify([[64], [65], [50], [157], [51], [86], [85], [135], [158], [89], [161], [136], [52], [66], [162]])
+    ex.extractAnnotaton(0, 'MT')
+    ex.timeModify([[64, 65, 66], [50, 51, 52], [157, 158], [85, 86], [135], [89], [161, 162], [136]], 1)
 
     ex.extractAnnotaton(0, 'MT')
 
     #  7_14
     ex = extractHG('7_14')
-    # ex.timeModify([63, 178, 174, 189, 80, 179, 188, 62, 109, 177, 140, 64, 175, 110])
-    ex.timeModify([[62, 63, 64], [177, 178, 179], [174, 175], [188, 189], [80], [109, 110], [140]])
+    ex.timeModify([[63], [178], [174], [189], [80], [179], [188], [62], [109], [177], [140], [64], [175], [110]])
+    ex.extractAnnotaton(0, 'HITS')
+    ex.timeModify([[62, 63, 64], [177, 178, 179], [174, 175], [188, 189], [80], [109, 110], [140]], 1)
 
     ex.extractAnnotaton(0, 'HITS')
 
-    # ex.timeModify([63, 178, 174, 179, 189, 62, 54, 64, 80, 188, 53, 110, 109, 177, 159])
-    ex.timeModify([[62, 63, 64], [177, 178, 179], [174], [188, 189], [53, 54], [80], [109, 110], [159]])
+    ex.timeModify([[63], [178], [174], [179], [189], [62], [54], [64], [80], [188], [53], [110], [109], [177], [159]])
+    ex.extractAnnotaton(0, 'MT')
+    ex.timeModify([[62, 63, 64], [177, 178, 179], [174], [188, 189], [53, 54], [80], [109, 110], [159]], 1)
 
     ex.extractAnnotaton(0, 'MT')
